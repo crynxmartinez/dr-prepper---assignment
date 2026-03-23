0f257b15a0bcc2524c455faf931f4b1f9ab31818
crynxmartinez <crynxmartinez@github.com>
2026-03-23 17:14:17 +0800

    Fix Vite build: Add root index.html and configure publicDir

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  publicDir: false, // Don't copy public folder during build (Express already serves it)
   build: {
     outDir: 'public',
-    emptyOutDir: false,
+    emptyOutDir: false, // Don't delete existing files in public/
     rollupOptions: {
       input: './index.html'
     }
